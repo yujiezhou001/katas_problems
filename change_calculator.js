c4fe0e968da2bf371c6a53665fc67db064dd2f19
Gordon <gordonzhou68@outlook.com>
2019-08-22 22:56:09 -0400

    change_calcultor katas,  define all constants

---
+++ change_calculator.js
@@ -0,0 +1,20 @@
+const twentyDollar = 2000;
+const tenDollar = 1000;
+const fiveDollar = 500;
+const twoDollar = 200;
+const oneDollar = 100;
+const quarter = 25;
+const dime = 10; 
+const nickel = 5;
+const penny = 1;
+const calculateChange = function(total, cash) {
+
+};
+  
+// console.log(calculateChange(1787, 2000));
+// console.log(calculateChange(2623, 4000));
+// console.log(calculateChange(501, 1000));
+
+// { twoDollar: 1, dime: 1, penny: 3 }
+// { tenDollar: 1, twoDollar: 1, oneDollar: 1, quarter: 3, penny: 2 }
+// { twoDollar: 2, quarter: 3, dime: 2, penny: 4 }
