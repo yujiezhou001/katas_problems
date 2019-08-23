6bef86c8fffba54781e8541f0759105a7f772685
Gordon <gordonzhou68@outlook.com>
2019-08-22 23:08:26 -0400

    check remainders

--- change_calculator.js
+++ change_calculator.js
@@ -1,3 +1,5 @@
+// define all constants amount for bills
+
 const twentyDollar = 2000;
 const tenDollar = 1000;
 const fiveDollar = 500;
@@ -7,11 +9,40 @@ const quarter = 25;
 const dime = 10; 
 const nickel = 5;
 const penny = 1;
+
+const billAmount = {
+    twentyDollar: 2000,
+    tenDollar: 1000,
+    fiveDollar: 500,
+    twoDollar: 200,
+    oneDollar: 100,
+    quarter: 25,
+    dime: 10,
+    nickel: 5,
+    penny: 1
+}
+
+// Main function
+
 const calculateChange = function(total, cash) {
+    const output = {};
+    const diff = total - cash;
+    //223
+    console.log(diff % quarter)
+    // switch(diff) {
+    //     case x:
+    //       // code block
+    //       break;
+    //     case y:
+    //       // code block
+    //       break;
+    //     default:
+    //       // code block
+    //   }
 
 };
   
-// console.log(calculateChange(1787, 2000));
+calculateChange(1787, 2000);
 // console.log(calculateChange(2623, 4000));
 // console.log(calculateChange(501, 1000));
 
